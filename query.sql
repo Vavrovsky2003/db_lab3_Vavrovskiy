01c7870244880aa85c35f912c8cac5d8e81bf2df
Vavrovsky2003 <113840563+Vavrovsky2003@users.noreply.github.com>
2023-12-31 00:05:43 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,23 +1,24 @@
---1. Кількість рейсів відправлених з San Francisco International Airport
-Select count(airline) From flight_route 
+--1. Вивести кількість рейсів відправлених з San Francisco International Airport
+Select count(flight_id) From flight_route 
 Where Origin_Airport In (
-    Select IATA_code From Origin_Airport 
-    Where name='San Francisco International Airport'
+    Select Airport_id  From Airport 
+    Where Airport_name='San Francisco International Airport'
     )
 
---2. Вивести аероперевізників, літаки яких, які прибували в аеропорт Х'юстона 
-Select name From Airline 
-Where IATA_code In (
+--2. Вивести повні назви аероперевізників, літаки яких, які прибували в аеропорт Х'юстона 
+Select Airline_name From Airline 
+Where Airline_id In (
     Select Airline From Flight_Route 
     Where Destination_Airport='IAH'
     )
 
 --3. Вивести повну назву аеропорта призначення рейса 2336 аероперевізника American Airlines Inc.
-Select name From Destination_Airport 
-Where IATA_code In (
+Select Airport_name From Airport 
+Where Airport_id In (
     Select Distinct(Destination_Airport) 
     From Flight_Route Join Airline on 
-    Flight_Route.airline=Airline.IATA_code 
+    Flight_Route.airline=Airline.Airline_id
     Where Flight_Number=2336 
-    And name='American Airlines Inc.')  
+    And Airline_name='American Airlines Inc.')  
+
 
